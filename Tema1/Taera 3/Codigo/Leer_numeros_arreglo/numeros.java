6218554abc7ff7008682cf0b0236727918ad0b43
David Patricio Escamilla Marquez <46275040+MagoPato@users.noreply.github.com>
2021-08-26 11:48:52 -0500

    Tarea 3 Arreglo

---
+++ Tema1/Taera 3/Codigo/Leer_numeros_arreglo/numeros.java
@@ -0,0 +1,34 @@
+package Leer_numeros_arreglo;
+
+import java.util.Scanner;
+
+class numeros {
+    private final double numeros[] = new double[10];
+    private double suma=0;
+    Scanner teclado = new Scanner(System.in);
+
+    public numeros() {
+        this.suma=0;
+    }
+
+    public void predirDatos() {
+        int n = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            n++;
+            System.out.println("dime el numero " + n);
+            numeros[i] = teclado.nextDouble();
+        }
+    }
+
+    public void sumaimpares() {
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i]%2 !=0){
+               suma+=numeros[i];
+            }
+        }
+    }
+    
+    public double getSuma(){
+        return suma;
+    }
+}
---
+++ Tema1/Taera 3/Netbeans/Leer_numeros_arreglo/src/Leer_numeros_arreglo/Leer_numeros_arreglo.java
@@ -0,0 +1,18 @@
+/*
+       poo
+       Leer 10 valores desde el teclado y almacenarlos en un arreglo 
+       unidimensional, se desea calcular e imprimir la suma de los 
+       valores impares almacenados en el arreglo
+ */
+package Leer_numeros_arreglo;
+
+public class Leer_numeros_arreglo {
+
+    public static void main(String[] args) {
+        numeros numeros = new numeros();
+        numeros.predirDatos();
+        numeros.sumaimpares();
+        System.out.println("la suma de los numeros impares es "+numeros.getSuma());
+    }
+    
+}
