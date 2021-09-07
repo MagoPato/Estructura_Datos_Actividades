5d96363dc7fd194c3dc87fa167ff34e908be8db3
David Patricio Escamilla Marquez <46275040+MagoPato@users.noreply.github.com>
2021-09-07 10:38:23 -0500

    tarea 1 _ tema 2

---
+++ Tema 2/Taera 1/Codigo/recursi_uniddad2/Fibonacci.java
@@ -0,0 +1,40 @@
+package recursi_uniddad2;
+
+public class Fibonacci {
+
+    private int tam;
+
+    public Fibonacci(int tam) {
+        this.tam = tam;
+    }
+
+    public Fibonacci() {
+        this.tam = 0;
+    }
+
+    void mostraSerie() {
+        for (int i = 0; i <= getTam(); i++) {
+            System.out.print(resolucion(i) + " ");
+        }
+    }
+
+    void mostraPosicion() {
+        System.out.println("\n" + resolucion(getTam()));
+    }
+
+    int resolucion(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return resolucion(n - 1) + resolucion(n - 2);
+    }
+
+    public int getTam() {
+        return tam;
+    }
+
+    public void setTam(int tam) {
+        this.tam = tam;
+    }
+
+}
---
+++ Tema 2/Taera 1/Codigo/recursi_uniddad2/Recursi_uniddad2.java
@@ -0,0 +1,23 @@
+package recursi_uniddad2;
+
+import java.util.Scanner;
+
+public class Recursi_uniddad2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Fibonacci Fibonacci = new Fibonacci();
+        Scanner teclado = new Scanner(System.in);
+        int n;
+        System.out.println("dime hasta que posicion quieres ver la seria de Fibonacci");
+        n = teclado.nextInt();
+        if (n > 0) {
+            Fibonacci.setTam(n);
+            Fibonacci.mostraSerie();
+        } else {
+            System.out.println("pone numeros mayores a 0");
+        }
+
+    }
+}
